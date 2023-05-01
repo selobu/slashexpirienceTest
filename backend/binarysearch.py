90eeb4b1400ab031b1da44a071133e9272de2947
Sebastian Lopez Buritica <selobu@gmail.com>
2023-05-01 05:39:35 -0500

    refactor: :recycle: Control functions to be exported, remove test, rename variable

--- backend/binarysearch.py
+++ backend/binarysearch.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env
+__all__ = ["get_next_positions", "getmiddle_pos_and_value"]
+
 from readdata import readValueData
 from random import randrange
 from configuration import saveConfig, config
@@ -22,7 +24,7 @@ def _getmiddle(initpos: int, endpos: int) -> int:
     return middlepos
 
 
-def _getmiddle_pos_and_value(
+def getmiddle_pos_and_value(
     initpos: int = 1, endpos: int = MAX_POSITION_ALLOWED
 ) -> int:
     """reads the middlepos and value
@@ -73,7 +75,7 @@ def test():
     print("Think a value beetween 1 and 61961")
     initpos = 0
     endpos = MAX_POSITION_ALLOWED
-    middlepos, middlevalue = _getmiddle_pos_and_value(initpos, endpos)
+    middlepos, middlevalue = getmiddle_pos_and_value(initpos, endpos)
     print("press 1 if true else any value")
     tryes = 0
     maxtryes = MAX_TRIES
@@ -92,27 +94,6 @@ def test():
             print("Not found")
             return selectecValue, middlevalue, tryes
 
-        initpos, endpos = res["initpos"], res["endpos"]
-        middlepos, middlevalue = _getmiddle_pos_and_value(initpos, endpos)
-
-
-def auto_test(selectedValue):
-    """Auto test random valu"""
-    initpos = 0
-    endpos = MAX_POSITION_ALLOWED
-    middlepos, middlevalue = _getmiddle_pos_and_value(initpos, endpos)
-    tryes = 0
-    maxtryes = 16
-    while True:
-        tryes += 1
-        condition = [0, "1"][selectecValue > middlevalue]
-        res = get_next_positions(condition == "1", middlepos, initpos, endpos)
-        if endpos == middlepos or initpos == middlepos:
-            middlepos = [initpos, endpos][condition == "1"]
-            middlevalue = readValueData(middlepos)
-            return selectecValue, middlevalue, tryes
-        if tryes > maxtryes + 1:
-            return selectecValue, middlevalue, tryes
         initpos, endpos = res["initpos"], res["endpos"]
         middlepos, middlevalue = _getmiddle_pos_and_value(initpos, endpos)
 
